446d81bfbe4c921c059c874d7e9436dee411ab4f
Spencer <spencer@willowisp.us>
2021-01-05 11:57:27 -0500

    reverting changes to components

---
+++ __tests__/enzyme.js
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import toJson from 'enzyme-to-json';
+// import LineChart from '../client/components/lineChart';
+
+// configure({ adapter: new Adapter() });
+
+// describe('React unit tests', () => {
+//     desribe('LineChart', () => {
+//         beforeAll(() => {
+//             wrapper = shallow(<LineChart/>);
+//         });
+
+//         it('Renders a <div> tag with an id of "mainchart"', () => {
+//             expect wrapper.type().toEqual('div'); 
+        
+//         })
+
+//     }); 
+
+//     describe('BarChart', () => {
+//         beforeAll(() => {
+//             wrapper = shallow(<BarChart/>);
+//         });
+
+//         if()
+//         expect wrapper.type().toEqual('div')
+
+//     })
+    
+
+// }); 
