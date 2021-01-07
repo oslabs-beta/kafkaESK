574cd3eeeb54fdf7a242189e325fbc59a2926f82
Spencer <spencer@willowisp.us>
2021-01-06 20:24:03 -0500

    enzyme tests for React components

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,33 +1,66 @@
-// import React from 'react';
-// import { configure, shallow } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
-// import LineChart from '../client/components/lineChart';
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import LineChart from '../client/components/LineChart';
+import DoughnutChart from '../client/components/DoughnutChart';
+import BarChart from '../client/components/BarChart';
+import PolarChart from '../client/components/PolarChart';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+configure({ disableLifecycleMethods: true });
 
-// describe('React unit tests', () => {
-//     desribe('LineChart', () => {
-//         beforeAll(() => {
-//             wrapper = shallow(<LineChart/>);
-//         });
+describe('React unit tests', () => {
 
-//         it('Renders a <div> tag with an id of "mainchart"', () => {
-//             expect wrapper.type().toEqual('div'); 
-        
-//         })
+    let wrapper; 
+    let props = {
+        data: "data"
+    }; 
 
-//     }); 
+    describe('LineChart', () => {
 
-//     describe('BarChart', () => {
-//         beforeAll(() => {
-//             wrapper = shallow(<BarChart/>);
-//         });
+        beforeAll(() => {
+            wrapper = shallow(<LineChart/>);
+        });
 
-//         if()
-//         expect wrapper.type().toEqual('div')
+        it('Renders component within a  <div> tag', () => {
+            expect(wrapper.type()).toEqual('div'); 
+         })
 
-//     })
-    
+        it ('Renders a canvas element for the line chart', () => {
+            expect(wrapper.find('canvas')); 
+        }); 
+    }); 
 
-// }); 
+    describe('DoughnutChart', () => {
+
+        beforeAll(() => {
+            wrapper = shallow(<DoughnutChart/>);
+        });
+
+        it ('Renders a canvas element for the doughnut chart', () => {
+            expect(wrapper.find('canvas')); 
+         })
+    }); 
+
+    describe('PolarChart', () => {
+
+        beforeAll(() => {
+            wrapper = shallow(<PolarChart/>);
+        });
+
+        it ('Renders a canvas element for the polar chart', () => {
+            expect(wrapper.find('canvas')); 
+        })
+    }); 
+
+    describe('BarChart', () => {
+
+        beforeAll(() => {
+            wrapper = shallow(<BarChart/>);
+        });
+
+        it ('Renders a canvas element for the line chart', () => {
+            expect(wrapper.find('canvas')); 
+        }); 
+    }); 
+}); 
